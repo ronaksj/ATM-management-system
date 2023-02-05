b341edab1890862043b1ffd5504a846317d7b5c7
Ronak Jagani <116540524+ronaksj@users.noreply.github.com>
2023-02-05 16:47:06 +0530

    Delete src directory

--- src/check.java
+++
@@ -1,8 +0,0 @@
-public class check {
-    static void checkbalance() {
-        int b = ATM.display_balance();
-        System.out.println("The available balance in your account is :" + b);
-        System.out.println("-------------------------------------------");
-        ATM.select_operation();
-    }
-}
