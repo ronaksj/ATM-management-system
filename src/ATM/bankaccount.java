0c85cf43a4ada225db06c607d898b57cae14f5bd
Ronak Jagani <116540524+ronaksj@users.noreply.github.com>
2023-02-07 20:39:57 +0530

    Update bankaccount.java

--- src/ATM/bankaccount.java
+++ src/ATM/bankaccount.java
@@ -8,10 +8,10 @@ public class bankaccount {
         ATM.name = sc.nextLine();
 
         System.out.println("Enter username :");
-        String user = sc.nextLine();
+        sc.nextLine();
 
         System.out.println("Enter your password :");
-        String pass = sc.nextLine();
+        sc.nextLine();
 
         System.out.println("Enter your Account number :");
         ATM.account_number = sc.nextLine();
@@ -19,25 +19,5 @@ public class bankaccount {
         System.out.println("REGISTRATION SUCCESSFULLY!");
         System.out.println("***************************");
         ATM.select_operation();
-
-        while (true) {
-            display(ATM.name);
-            int choice = sc.nextInt();
-            if (choice == 1) {
-                login(user, pass);
-                break;
-            } 
-            else {
-                if (choice == 2) {
-                    System.exit(0);
-                } 
-                else {
-                    System.out.println("Bad value! Enter again!");
-                }
-            }
-        }
     }
-
-    static void display(String name) {}
-    static void login(String user, String pass) {}
 }
